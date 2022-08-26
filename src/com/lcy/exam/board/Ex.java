0197712bb65a599e6c213517a5d2ac4a6c11ce68
chship <dldidsla13@naver.com>
2022-08-27 00:37:59 +0900

    세팅

--- src/com/lcy/exam/board/Ex.java
+++ src/com/lcy/exam/board/Ex.java
@@ -1,8 +1,7 @@
 package com.lcy.exam.board;
 
-public class Main {
+public class Ex {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
