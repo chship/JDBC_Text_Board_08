80e4e790132bebe93bc046d53a4047f9d5b5a420
chship <dldidsla13@naver.com>
2022-08-29 20:05:34 +0900

    게시물 작성시 DB저장

--- src/com/lcy/exam/board/App.java
+++ src/com/lcy/exam/board/App.java
@@ -1,50 +1,89 @@
 package com.lcy.exam.board;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class App {
     public void run() {
         Scanner sc = Container.scanner;
         List<Article> articles = new ArrayList<>();
         int articleLastId = 0;
-
         while (true) {
             System.out.printf("명령어) ");
             String cmd = sc.nextLine().trim();
-            if (cmd.equals("/usr/article/write")) {
+            if(cmd.equals("/usr/article/write")) {
                 System.out.println("== 게시물 등록 ==");
                 System.out.printf("제목 : ");
                 String title = sc.nextLine();
                 System.out.printf("내용 : ");
                 String body = sc.nextLine();
 
-
                 int id = ++articleLastId;
+
+                Connection conn = null;
+                PreparedStatement pstat = null;
+
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+
+                    String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+                    conn = DriverManager.getConnection(url, "sbsst", "sbs123414");
+
+                    String sql = "INSERT INTO article";
+                    sql += " SET regDate = NOW()";
+                    sql += ", updateDate = NOW()";
+                    sql += ", title = \"" + title + "\"";
+                    sql += ", `body` = \"" + body + "\";";
+
+                    pstat = conn.prepareStatement(sql);
+                    pstat.executeUpdate();
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패");
+                } catch (SQLException e) {
+                    System.out.println("에러: " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstat != null && !pstat.isClosed()) {
+                            pstat.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
 
                 Article article = new Article(id, title, body);
 
                 articles.add(article);
-
                 System.out.println("생성된 게시물 객체 : " + article);
                 System.out.printf("%d번 게시물이 등록 되었습니다.\n", article.id);
-
-            } else if (cmd.equals("/usr/article/list")) {
+            }
+            else if(cmd.equals("/usr/article/list")) {
                 System.out.println("== 게시물 리스트 ==");
-
-                if (articles.size() == 0) {
+                if( articles.size() == 0 ) {
                     System.out.println("게시물이 존재하지 않습니다.");
                     continue;
                 }
-
                 System.out.println("번호 / 제목");
-
-                for (Article article : articles) {
+                for( Article article : articles ) {
                     System.out.printf("%d / %s\n", article.id, article.title);
                 }
-
-            } else if (cmd.equals("exit")) {
+            }
+            else if(cmd.equals("exit")) {
                 System.out.println("== 시스템 종료 ==");
                 break;
             }
