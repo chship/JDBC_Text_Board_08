d00e76ea61023390ba72100ce466c68729b29648
chship <dldidsla13@naver.com>
2022-08-29 21:23:23 +0900

    JDBC SELECT 테스트

--- src/com/lcy/exam/board/Article.java
+++ src/com/lcy/exam/board/Article.java
@@ -2,17 +2,29 @@ package com.lcy.exam.board;
 
 public class Article {
     int id;
+    String regDate;
+    String updateDate;
     String title;
     String body;
 
-    Article(int id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
+  public Article(int id, String regDate, String updateDate, String title, String body) {
+            this.id = id;
+            this.regDate = regDate;
+            this.updateDate = updateDate;
+            this.title = title;
+            this.body = body;
+        }
 
-    @Override
-    public String toString() {
-        return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
+
+  public Article(int id, String title, String body) {
+            this.id = id;
+            this.title = title;
+            this.body = body;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("{id : %d, regDate : %s, updateDate : %s, title : \"%s\", body : \"%s\"}",
+                    id, regDate, updateDate, title, body);
+        }
     }
-}
